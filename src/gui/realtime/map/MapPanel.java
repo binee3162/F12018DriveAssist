16116079339604a3b8cf9e65b0f134b65fba3b55
Song <binee3162@gmail.com>
2020-03-11 16:47:38 +0100

    first version

--- src/gui/realtime/map/MapPanel.java
+++ src/gui/realtime/map/MapPanel.java
@@ -17,28 +17,36 @@ public class MapPanel extends JPanel {
         this.setBackground(new Color(0,0,0,0));
 
         readTrackFile(trackId);
+
     }
     //paint when creating panel, use updateUI to fresh content of panel.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawMap(g);
+       // Graphics2D g2d=(Graphics2D) g.create();
+        //g2d.rotate(90);
+
     }
-    private void drawMap(Graphics g){
-  //todo: draw map not always in full scale
-        int[] first=null;
-        for(int[] coord: trackCoords) {
-            if(first==null)
-                first=coord;
+    private void drawMap(Graphics g) {
+        //todo: draw map not always in full scale
+        int[] first = null;
+        for (int[] coord : trackCoords) {
+            if (first == null)
+                first = coord;
             else {
                 //System.out.println("z: " + coord[0] + " x: " + coord[1]);
-                Graphics2D g2d=(Graphics2D) g.create();
+                Graphics2D g2d = (Graphics2D) g.create();
+                g2d.rotate(0, 300, 300);  //todo:figure out 是挪画布还是画点
+
                 g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 g2d.setColor(Color.RED);
-                BasicStroke bs=new BasicStroke(2);
+                BasicStroke bs = new BasicStroke(2);
                 g2d.setStroke(bs);
-                g2d.drawLine(first[1]/8+100,first[0]/8+100,coord[1]/8+100,coord[0]/8+100);
-                first=coord;
+                //g2d.drawLine(first[1]/8+100,first[0]/8+100,coord[1]/8+100,coord[0]/8+100);
+                g2d.drawLine(first[1] / 4 + 200, first[0] / 4 + 200, coord[1] / 4 + 200, coord[0] / 4 + 200);
+                first = coord;
+
             }
         }
     }
@@ -47,7 +55,8 @@ public class MapPanel extends JPanel {
     public void readTrackFile(int trackId) {
         //read file and store in List
         //String fileName = "./tracks/" + String.valueOf(trackId) + ".track";
-        String fileName="C:\\Users\\Song\\Desktop\\Lecture\\EE5\\formula4\\src\\gui\\realtime\\map\\tracks\\"+trackId+".track";
+        //String fileName="C:\\Users\\Song\\Desktop\\Lecture\\EE5\\formula4\\src\\gui\\realtime\\map\\tracks\\"+trackId+".track";
+        String fileName="C:\\Users\\Song\\Desktop\\Lecture\\EE5\\formula4\\src\\gui\\realtime\\map\\tracks\\Abu_Dhabi_outerlimit.track";
         try {
             Scanner sc = new Scanner(new File(fileName));
             //first read data that is not important
@@ -70,4 +79,6 @@ public class MapPanel extends JPanel {
             System.out.println("File " + fileName + " not found");
         }
     }
+
+
 }
